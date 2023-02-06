897965b71a92a033ff0ebcce66a45e3ba7b66b68
ZeeoneOfficial <92008777+ZeeoneOfficial@users.noreply.github.com>
2023-02-06 15:48:11 +0800

    Update gitprofile.config.js

--- gitprofile.config.js
+++ gitprofile.config.js
@@ -2,7 +2,7 @@
 
 const config = {
   github: {
-    username: 'zeeone-ofc', // Your GitHub org/user name. (Required)
+    username: 'zeeoneofficial', // Your GitHub org/user name. (Required)
     sortBy: 'stars', // stars | updated
     limit: 10, // How many projects to display.
     exclude: {
