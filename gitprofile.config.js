7dfb503730282c4e77c3ce6fca16fa8eb767cb9c
ZeeoneOfc's 3rd account <92008777+zeeoneofficial2@users.noreply.github.com>
2023-02-08 07:18:31 +0800

    Update gitprofile.config.js

--- gitprofile.config.js
+++ gitprofile.config.js
@@ -2,7 +2,7 @@
 
 const config = {
   github: {
-    username: 'zeeoneofficial', // Your GitHub org/user name. (Required)
+    username: 'zeeoneofficial2', // Your GitHub org/user name. (Required)
     sortBy: 'stars', // stars | updated
     limit: 10, // How many projects to display.
     exclude: {
